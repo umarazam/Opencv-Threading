fa78a475f012bc14ea22c1c2b651794801c1641d
umarazam <umarazam01@gmail.com>
2020-08-23 18:06:38 +0500

    Resolved the issue now it is working with proper multithreading

--- frameQueue.cpp
+++ frameQueue.cpp
@@ -10,6 +10,7 @@
 // Hiccups
 	// Camera input is not working tested on both python and cpp
 	
+std::mutex m;
 int producer( std::queue<cv::Mat> &queue){
 	cv::Mat frame; // Matrix
 	cv::VideoCapture capture("../resources/people-walking.mp4"); // Getting feed
@@ -21,27 +22,46 @@ int producer( std::queue<cv::Mat> &queue){
 	while (true){
 		capture.read(frame);
 		queue.push(frame);
+		/**
 		if (frame.empty()){
 			std::cerr << "Error, blank frame grabbed\n";
 			break;
 		}
+		**/
 
 		cv::imshow("Live", frame);
 
 		if (cv::waitKey(20) >=0)
 			break;
 	}
+	return 0;
 }
 
-int consumer(std::queue<cv::Mat> &queue){
+void consumer(std::queue<cv::Mat> &queue){
+	int choice = -99;
+	while (true){
+	std::cout << "Want to look at the frame then press 1: ";
+	std::cin >> choice;
+	if (choice == 1){
+		choice = -99;
+		m.lock();
+		cv::Mat img = queue.front();
+		m.unlock();
+		cv::imshow("Pop", img);
+		//cv::waitKey(0);
+		}
+	}
 }
 
 int run(){
 	std::queue<cv::Mat> queue; 
+	while (true){
 	std::thread prod(producer, std::ref(queue)); 
+	std::thread cons(consumer, std::ref(queue));
+
 	prod.join();
-	cv::Mat img = queue.front();
-	cv::imshow("Pop", img);
-	cv::waitKey(0);
+	cons.join();
+	}
+		
 	return 0;
 }
